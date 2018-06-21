f68f727bdfd45a687be55fe742fc9f6b598a3314
SaiVaibhav <sai.vaibhav@target.com>
2018-06-21 15:31:40 +0530

    practice

--- complete/src/main/java/com/model/Trip.java
+++ complete/src/main/java/com/model/Trip.java
@@ -1,17 +1,25 @@
 package com.model;
 
-/**
- * Created by z002qz1 on 6/20/18.
- */
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Min;
 
 @Data
 public class Trip
 {
+    @JsonProperty("driver")
     private String driverId;
+    @JsonProperty("customer")
     private String customerId;
+    @JsonProperty("driverRating")
+    @Range(min = 0, max = 5, message = "rating has to be a value between 0 and 5")
     private Integer driverRating;
+    @JsonProperty("customerRating")
+    @Range(min = 0, max = 5, message = "rating has to be a value between 0 and 5")
     private Integer customerRating;
     private String tripId;
 
--- complete/src/main/java/com/repository/TripRepo.java
+++ complete/src/main/java/com/repository/TripRepo.java
@@ -4,9 +4,7 @@ import com.model.Trip;
 
 import java.util.List;
 
-/**
- * Created by z002qz1 on 6/20/18.
- */
+
 public class TripRepo
 {
     private static List<Trip> listOfTrips;
